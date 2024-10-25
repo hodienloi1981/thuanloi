1ba972018beb4b16fa61a64b6ddbc0a0532a20e4
HoDienLoi <hodienloi@gmail.com>
2024-10-25 10:07:05 +0700

    baitap

---
+++ js/index.js
@@ -0,0 +1,72 @@
+const list_link = [
+    { 'name': 'Danh sách box shadow', 'link': 'box-shadow.html', 'thumbnail': 'images/box-shadow.png' },
+    { 'name': 'Hướng dẫn git', 'link': 'huong-dan-git.html', 'thumbnail': 'images/huong-dan-git.png' },
+    { 'name': '10015.io', 'link': 'https://10015.io/', 'thumbnail': 'images/huong-dan-git.png' },
+    { 'name': 'fancy-border-radius', 'link': 'https://9elements.github.io/fancy-border-radius/', 'thumbnail': 'images/huong-dan-git.png' },
+    { 'name': '100dayscss.com', 'link': 'https://100dayscss.com', 'thumbnail': 'images/huong-dan-git.png' },
+    { 'name': 'uiverse.io', 'link': 'https://uiverse.io/', 'thumbnail': 'images/huong-dan-git.png' },
+]
+const list_baitap = [
+    { 'name': 'Bài tập 1', 'link': 'baitap/bai1.html', 'thumbnail': 'images/bai1.png' },
+    { 'name': 'Bài tập 2', 'link': 'baitap/bai2.html', 'thumbnail': 'images/bai2.png' },
+    { 'name': 'Bài tập 3', 'link': 'baitap/bai3.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 4', 'link': 'baitap/bai4.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 5', 'link': 'baitap/bai5.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 6', 'link': 'baitap/bai6.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 7', 'link': 'baitap/bai7.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 8', 'link': 'baitap/bai8.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 9', 'link': 'baitap/bai9.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 10', 'link': 'baitap/bai10.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 11', 'link': 'baitap/bai11.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 12', 'link': 'baitap/bai12.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 13', 'link': 'baitap/bai13.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 14', 'link': 'baitap/bai14.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 15', 'link': 'baitap/bai15.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 16', 'link': 'baitap/bai16.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 17', 'link': 'baitap/bai17.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 18', 'link': 'baitap/bai18.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 19', 'link': 'baitap/bai19.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 20', 'link': 'baitap/bai20.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 21', 'link': 'baitap/bai21.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 22', 'link': 'baitap/bai22.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 23', 'link': 'baitap/bai23.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 24', 'link': 'baitap/bai24.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 25', 'link': 'baitap/bai25.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 26', 'link': 'baitap/bai26.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 27', 'link': 'baitap/bai27.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 28', 'link': 'baitap/bai28.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 29', 'link': 'baitap/bai29.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 30', 'link': 'baitap/bai30.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 31', 'link': 'baitap/bai31.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 32', 'link': 'baitap/bai32.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 33', 'link': 'baitap/bai33.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 34', 'link': 'baitap/bai34.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 35', 'link': 'baitap/bai35.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 36', 'link': 'baitap/bai36.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 37', 'link': 'baitap/bai37.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 38', 'link': 'baitap/bai38.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 39', 'link': 'baitap/bai39.html', 'thumbnail': 'images/bai1.jpg' },
+    { 'name': 'Bài tập 40', 'link': 'baitap/bai40.html', 'thumbnail': 'images/bai1.jpg' },
+
+]
+var list_link_rowdata = ``;
+list_link.forEach(function (item, index) {
+    list_link_rowdata += `<li><a href="${item.link}">${item.name}</a></li>`;
+});
+document.getElementById("list_website").innerHTML = list_link_rowdata;
+
+
+var list_baitap_rowdata = ``;
+list_baitap.forEach(function (item, index) {
+    list_baitap_rowdata += `
+        <div class="col-6 col-md-3 mb-3">
+            <div class="border">
+                <a href="${item.link}">
+                    <img class="img-fluid w-100" src="${item.thumbnail}" />
+                    <h3 class="text-center fs-5 py-2">${item.name}</h3>
+                </a>
+            </div>
+        </div>
+    `;
+});
+document.getElementById("list_baitap").innerHTML = list_baitap_rowdata;
